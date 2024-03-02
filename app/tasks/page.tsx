80e293584072552af773a8548ffeccd5e544d224
dataipov <dmitry@hypha.cc>
2024-03-02 17:01:26 +0600

    Add postgres. Fetch data from a table

---
+++ app/tasks/page.tsx
@@ -0,0 +1,19 @@
+import { sql } from "@vercel/postgres";
+
+export default async function Tasks({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from TASK`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.name} - {row.description}
+        </div>
+      ))}
+    </div>
+  );
+}
